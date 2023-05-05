681fa2d5a39503da43b1cf29728620d4e83195f9
amits-whizit <129951022+amits-whizit@users.noreply.github.com>
2023-05-05 12:56:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Amit Bhau Zindabad
+          WHIZIT SERVICES
         </a>
       </header>
     </div>
