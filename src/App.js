2769c2d8d4fa68a39eb0cf7a88279e77254e5167
amits-whizit <129951022+amits-whizit@users.noreply.github.com>
2023-05-05 12:51:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Amit Bhau Zindabad
         </a>
       </header>
     </div>
